4db0e4f11d78adec72a0548ba9935f77159b7a23
Meeth Patel <meeth2060@gmail.com>
2025-07-22 13:23:26 +0530

    homie added

--- src/app/components/Appbar.tsx
+++ src/app/components/Appbar.tsx
@@ -1,12 +1,18 @@
-'use client'
+"use client";
+import Link from "next/link";
 
-export default function Appbar(){
-
-    return(
-        <div>
-          
-
-           
-        </div>
-    )
+export default function Appbar() {
+  return (
+    <div className="flex justify-between  items-center w-[600px]  ">
+      <div>Fake landing </div>
+      <div>
+        <li>
+            <ul><Link href={"/"}>Home</Link></ul>
+          <ul><Link href={"/"}>Dummy</Link></ul>
+             <ul><Link href={"/"}>Home</Link></ul>
+        </li>
+      </div>
+    </div>
+  );
 }
+
---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+export default function HeroSection(){
+
+    return(
+        <div className="">
+           <div className="font-extrabold text-white-35">
+             Dummy hero 
+           </div>
+           <div>
+            best ai related solutions , we are the best 
+           </div>
+        </div>
+    )
+}
