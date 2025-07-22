c83600bec0833ca591190708a1136851a18c3e69
Meeth Patel <meeth2060@gmail.com>
2025-07-22 18:47:04 +0530

    shipping with bugs like a pro

--- src/app/components/Appbar.tsx
+++ src/app/components/Appbar.tsx
@@ -5,7 +5,10 @@ export default function Appbar() {
   return (
     <div className="flex justify-between p-7     mx-auto   font-mono font-sans items-center  lg:w-[600px]  ">
       <div>Fake landing </div>
-      <div className="flex justify-center gap-x-4 items-center ">
+      <div>
+        
+      </div>
+      <div className="flex justify-center gap-x-4 opacity-0 lg:opacity-100 items-center ">
        
             <ul><Link href={"/"}>Home</Link></ul>
           <ul><Link href={"/"}>Dummy</Link></ul>
