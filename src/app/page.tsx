12fbc12b25d9d436f93bfbf896009411e83f7312
Meeth Patel <meeth2060@gmail.com>
2025-07-22 19:58:48 +0530

    added ace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import HeroSection from "./components/Hero";
 import Card from "./components/Card";
+import { LampDemo } from "./components/lamp";
 
 export default function Home() {
   return (
@@ -8,6 +9,8 @@ export default function Home() {
       <div className="">
       <HeroSection />
       </div >
+
+      <LampDemo />
       <div className=" mx-auto w-[300px] gap-5 grid grid-cols-2 md:grid-2 lg:grid-cols-2">
        <Card description="We are reliablly fast" title="Quick" />
          <Card description="We are reliablly fast" title="Quick" />
