2e36c66787795fb5f99f041dc3c98bdb61722018
Meeth Patel <meeth2060@gmail.com>
2025-07-22 16:49:03 +0530

    how do masks work ?

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -5,13 +5,16 @@ export default function HeroSection(){
 
     return(
         <div className=" w-[400px] mx-auto py-8  h-[500px]">
-          <div className="">
-           <div className="font-sans text-[60px] text-white-35">
+          <div >
+
+           <div className="font-sans text-[60px] text-white-35 mask-gradient relative leading-relaxed ">
              Dummy hero 
            </div>
            <div className="text-white/35 font-mono  ">
             best ai related solutions , we are the best 
            </div>
+          
+
            </div>
         </div>
     )
